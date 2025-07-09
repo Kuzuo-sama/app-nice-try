3897814e276cd417d3a7393dca4307fe1cbbc5db
Kuzuo-sama <euricopinho04@gmail.com>
2025-07-09 02:43:20 +0100

    Update webViewer.js

--- src/utils/webViewer.js
+++ src/utils/webViewer.js
@@ -1,6 +1,7 @@
 const mineflayerViewer = require('prismarine-viewer').mineflayer;
 const bot = require('../../index');
-const port = require('../../config.json').webviewer_port;
+const port = process.env.PORT || require('../../config.json').webviewer_port;
+
 
 bot.once('spawn', () => {
 	mineflayerViewer(bot, { port: port, firstPerson: false });
